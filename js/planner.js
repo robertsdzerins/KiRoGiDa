f8909f149024e29ca8c40b6b5a02132bb696e942
robertsdzerins <roberts.dzerins_1@edu.rtu.lv>
2025-05-10 09:25:53 +0300

    Update planner.js

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,22 @@
+const planningForm = document.getElementById('planning-form');
+const tripFormSection = document.getElementById('trip-form');
+const tripResultsSection = document.getElementById('trip-results');
+
+planningForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+        
+    setTimeout(() => {
+        tripFormSection.style.display = 'none'; // paslept formu
+        tripResultsSection.style.display = 'block'; // paradit rezultatu
+    }, 500);
+
+    const editPlanButton = document.getElementById('edit-plan');
+
+    editPlanButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    tripResultsSection.style.display = 'none';
+    tripFormSection.style.display = 'block';
+});
+
+});
 
